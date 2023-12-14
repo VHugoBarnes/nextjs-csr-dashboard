c0bb0d13b06b6f8fe51e92dfe9f101edb5d610fa
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-13 19:40:10 -0600

    Create PokemonCard

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,3 +1,4 @@
+import { PokemonCard } from "@/pokemons/PokemonCard";
 import { PokemonResponse, SimplePokemon } from "@/pokemons/interfaces";
 import Image from "next/image";
 import Link from "next/link";
@@ -22,27 +23,10 @@ const page = async () => {
         Pokemons
       </h1>
 
-      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
         {
           pokemons.map((pokemon) => (
-            <div key={pokemon.id} className="flex flex-col bg-neutral-800 w-40 justify-center items-center rounded-lg space-y-4">
-              <Image
-                alt={pokemon.name}
-                width={80}
-                height={80}
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-                className="w-20 h-20 px-4 pt-6"
-              />
-              <p className="capitalize font-bold text-center text-neutral-200 px-4">
-                {pokemon.name}
-              </p>
-              <Link href={`/dashboard/pokemons/${pokemon.id}`} className="border border-white text-white rounded-full px-3 text-sm hover:scale-105 transition-all duration-200">
-                More info
-              </Link>
-              <div className="bg-white w-full flex justify-center items-center py-2 rounded-b-lg">
-                <FaRegHeart className="text-pink-500 w-5 h-5" />
-              </div>
-            </div>
+            <PokemonCard key={pokemon.id} pokemon={pokemon} />
           ))
         }
       </div>
---
+++ src/pokemons/PokemonCard.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import { SimplePokemon } from "./interfaces";
+import { FaRegHeart } from "react-icons/fa";
+
+export const PokemonCard = ({ pokemon }: { pokemon: SimplePokemon }) => {
+  return (
+    <div key={pokemon.id} className="flex flex-col bg-neutral-800 w-40 justify-center items-center rounded-lg space-y-4">
+      <Image
+        alt={pokemon.name}
+        width={80}
+        height={80}
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+        className="w-20 h-20 px-4 pt-6"
+        priority={false}
+      />
+      <p className="capitalize font-bold text-center text-neutral-200 px-4">
+        {pokemon.name}
+      </p>
+      <Link href={`/dashboard/pokemons/${pokemon.id}`} className="border border-white text-white rounded-full px-3 text-sm hover:scale-105 transition-all duration-200">
+        More info
+      </Link>
+      <div className="bg-white w-full flex justify-center items-center py-2 rounded-b-lg">
+        <FaRegHeart className="text-pink-500 w-5 h-5" />
+      </div>
+    </div>
+  );
+};
