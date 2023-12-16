524fce3cbadb54c42a1e4bf941a9f7a3fa8366cb
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-15 18:20:00 -0600

    create array from selector

--- src/components/FavoritePokemons.tsx
+++ src/components/FavoritePokemons.tsx
@@ -4,11 +4,11 @@ import React from "react";
 import { PokemonCard } from "./PokemonCard";
 
 export const FavoritePokemons = () => {
-  const pokemons = useAppSelector(state => state.pokemons);
+  const pokemons = useAppSelector(state => Object.values(state.pokemons));
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
       {
-        Object.values(pokemons).map((pokemon) => (
+        pokemons.map((pokemon) => (
           <PokemonCard
             key={pokemon.id}
             pokemon={pokemon}
